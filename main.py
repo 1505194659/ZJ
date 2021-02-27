ab0b732aae4ee1cc084b50fd451f4f118b061def
1505194659 <32003781+1505194659@users.noreply.github.com>
2021-02-27 15:25:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,9 @@ headers={
 r=requests.post("https://fyns.eduw.cn/stu/dk9.aspx",headers=headers)
 r=requests.post()
 r.encoding='utf-8'
-print(r.text)
+h=r.text
+data={'text':'hh','desp':h}
+headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36'
+}
+requests.post('https://sc.ftqq.com/SCU162308Tf0a5313c2ad0c45158a4f7bb0002a9c26038bc39782c1.send',headers=headers,data=data)
 
