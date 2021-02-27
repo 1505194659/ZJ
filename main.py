69fffe1472110a89e7b1fe82160f17a409f492fc
1505194659 <32003781+1505194659@users.noreply.github.com>
2021-02-27 11:32:00 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+import requests
+headers={
+    'cookie':'stuinfo=stuid=746D617407B8993C&stuxm=2EB177EBAD2D3D30&stubj=7285B8596DEF819F961F13D942957AE819DBB0DE49999A22; ASP.NET_SessionId=jx0wrfkhzn4afmf5vqopqkf5','origin':'https://fyns.eduw.cn','user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36'}
+r=requests.post("https://fyns.eduw.cn/stu/dk9.aspx",headers=headers)
+r=requests.post()
+r.encoding='utf-8'
+print(r.text)
+
