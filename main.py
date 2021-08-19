cf9723b08e058119621dc4220020db24e49b5521
1505194659 <32003781+1505194659@users.noreply.github.com>
2021-08-19 14:25:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
 import requests
-requests.post('https://sctapi.ftqq.com/SCT65261TnN9vI4jsKohOHP5aSmxqKo0s.send?')
+headers={'cookie':'stuinfo=stuid=746D617407B8993C&stuxm=2EB177EBAD2D3D30&stubj=7285B8596DEF819F961F13D942957AE819DBB0DE49999A22; ASP.NET_SessionId=yuwyikt3qlvj3kuxdrdmipgg','origin':'https://fyns.eduw.cn','user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36',"upgrade-insecure-requests": "1",'referer': 'https://fyns.eduw.cn/stu/dk9.aspx'}
+r=requests.post("https://dk.zjabc.edu.cn/stu/dk.aspx",headers=headers)
